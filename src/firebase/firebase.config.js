19987efb87b210d3144b60b5e8e7b773a8027c54
James Foley <38796013+jimbits@users.noreply.github.com>
2019-04-04 13:36:55 -0600

    config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,11 +1,1 @@
-import Firebase from "firebase"
-        const config = {
-        apiKey: "AIzaSyCtoCvAscTtco7wkzTZimB3A7GHegw_Rbk",
-        authDomain: "naitdirectory.firebaseapp.com",
-        databaseURL: "https://naitdirectory.firebaseio.com",
-        projectId: "naitdirectory",
-        storageBucket: "naitdirectory.appspot.com",
-        messagingSenderId: "897458123389"
-      };
-const firebase = Firebase.initializeApp(config);
-     export default firebase
+//config settings for firebase
